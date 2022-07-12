4658f3ef39d6fce17cb6599aabcafc5bbd366dd7
wangqi <wangqi@xinc818.group>
2022-07-12 15:20:30 +0800

    feature: security table schame.

--- tools/docker/mysql/init.d/iam.sql
+++ tools/docker/mysql/init.d/iam.sql
@@ -1,21 +1,121 @@
-create database if not exists iam default character set utf8mb4 collate utf8mb4_unicode_ci;
-use iam;
+create
+database if not exists iam default character set utf8mb4 collate utf8mb4_unicode_ci;
+use
+iam;
 
-/* security user */
+drop table if exists sec_dept;
+create table sec_dept
+(
+    id             bigint      not null auto_increment,
+    `parent_id`    bigint comment 'parent dept id, null means top dept',
+    `name`         varchar(32) not null,
+    `type`         varchar(4) comment 'type',
+    dept_id_path   varchar(256),
+    dept_name_path varchar(256),
+    status         varchar(4) comment 'status。0: disabled, 1: enabled',
+    remark         varchar(16),
+    sort           int(8) not null default 0,
+    creator        varchar(32),
+    create_time    datetime default current_timestamp,
+    editor         varchar(32),
+    update_time    datetime default current_timestamp on update current_timestamp,
+    primary key (id),
+    key (update_time)
+) engine = innodb;
+
 drop table if exists sec_user;
 create table sec_user
 (
-    id          bigint      not null auto_increment comment 'id',
-    username    varchar(32) not null comment '用户名',
-    password    varchar(64) not null comment '用户密码',
+    id          bigint      not null auto_increment,
+    username    varchar(32) not null comment 'user name',
+    password    varchar(64) not null comment 'user password',
     salt        varchar(16) not null comment 'random salt',
-    `type`      tinyint(4) comment '用户类型',
-    status      varchar(4)  not null comment '用户状态',
-    remark      varchar(16) comment '备注',
-    creator     varchar(32) comment '创建人',
-    create_time timestamp default current_timestamp comment '创建时间',
-    editor      varchar(32) comment '修改人',
-    update_time timestamp default current_timestamp on update current_timestamp comment '更新时间',
+    `type`      varchar(4) comment 'type',
+    status      varchar(4)  not null comment 'status. 0: disabled, 1: enabled',
+    remark      varchar(16),
+    creator     varchar(32),
+    create_time datetime default current_timestamp,
+    editor      varchar(32),
+    update_time datetime default current_timestamp on update current_timestamp,
     primary key (id),
     key (update_time)
-) engine = innodb comment = 'security-user';
+) engine = innodb;
+
+drop table if exists sec_user_dept_relation;
+create table sec_user_dept_relation
+(
+    id              bigint not null auto_increment,
+    user_id         bigint not null,
+    dept_id bigint not null,
+    creator         varchar(32),
+    create_time     datetime default current_timestamp,
+    editor          varchar(32),
+    update_time     datetime default current_timestamp on update current_timestamp,
+    primary key (id),
+    key (update_time)
+) engine = innodb;
+
+drop table if exists sec_role;
+create table sec_role
+(
+    id          bigint      not null auto_increment,
+    `name`      varchar(64) not null,
+    `type`      varchar(4) comment 'type. 0: admin, 1: normal',
+    status      varchar(4)  not null comment 'status. 0: disabled, 1: enabled',
+    remark      varchar(16),
+    creator     varchar(32),
+    create_time datetime default current_timestamp,
+    editor      varchar(32) comment '修改人',
+    update_time datetime default current_timestamp on update current_timestamp,
+    primary key (id),
+    key (update_time)
+) engine = innodb;
+
+drop table if exists sec_user_role_relation;
+create table sec_user_role_relation
+(
+    id          bigint not null auto_increment,
+    user_id     bigint not null,
+    role_id     bigint not null,
+    creator     varchar(32),
+    create_time datetime default current_timestamp,
+    editor      varchar(32),
+    update_time datetime default current_timestamp on update current_timestamp,
+    primary key (id),
+    key (update_time)
+) engine = innodb;
+
+drop table if exists sec_menu;
+create table `sec_menu`
+(
+    id             bigint       not null auto_increment,
+    parent_id      bigint comment 'parent menu id, null means top menu',
+    `name`         varchar(64)  not null,
+    `path`         varchar(256) not null,
+    `type`         varchar(4) comment 'type. 0: menu, 1: page, 2: button',
+    menu_id_path   varchar(256),
+    menu_name_path varchar(256),
+    status         varchar(4)   not null comment 'status. 0: disabled, 1: enabled',
+    remark         varchar(16),
+    sort           int(8) not null default 0,
+    creator        varchar(32),
+    create_time    datetime default current_timestamp,
+    editor         varchar(32),
+    update_time    datetime default current_timestamp on update current_timestamp,
+    primary key (id),
+    key (update_time)
+) engine = innodb;
+
+drop table if exists sec_role_menu_relation;
+create table sec_role_menu_relation
+(
+    id          bigint not null auto_increment,
+    role_id     bigint not null,
+    menu_id     bigint not null,
+    creator     varchar(32),
+    create_time datetime default current_timestamp,
+    editor      varchar(32),
+    update_time datetime default current_timestamp on update current_timestamp,
+    primary key (id),
+    key (update_time)
+) engine = innodb;
